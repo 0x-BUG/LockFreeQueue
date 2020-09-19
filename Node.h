4f00a9d030e2bd751c15fadbd4ed4be94923e306
sudden-death-bug <71544116+sudden-death-bug@users.noreply.github.com>
2020-09-19 19:35:30 +0800

    Update Node.h

--- Node.h
+++ Node.h
@@ -58,48 +58,28 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 namespace node_type
 {
 
-    template<typename T>
-    struct Node
+template<typename T>
+struct NodeWithHazardPointer
+{
+    T data_;
+    std::atomic<NodeWithHazardPointer*> next_;
+    std::atomic<NodeWithHazardPointer*> hpNext_;
+
+    NodeWithHazardPointer() : next_(nullptr), hpNext_(nullptr) {}
+    explicit NodeWithHazardPointer(const T& data)
+       : data_(data),
+         next_(nullptr),
+         hpNext_(nullptr)
     {
-        T data_;
-        Node* next_;
-
-        Node() : next_(nullptr) {}
-        explicit Node(const T& data)
-            : data_(data),
-            next_(nullptr)
-        {
-        }
-        explicit Node(T&& data)
-            : data_(std::move(data)),
-            next_(nullptr)
-        {
-        }
-        ~Node() {}
-    };
-
-    template<typename T>
-    struct NodeWithHazardPointer
+    }
+    explicit NodeWithHazardPointer(T&& data)
+       : data_(std::move(data)),
+         next_(nullptr),
+         hpNext_(nullptr)
     {
-        T data_;
-        std::atomic<NodeWithHazardPointer*> next_;
-        std::atomic<NodeWithHazardPointer*> hpNext_;
-
-        NodeWithHazardPointer() : next_(nullptr), hpNext_(nullptr) {}
-        explicit NodeWithHazardPointer(const T& data)
-            : data_(data),
-            next_(nullptr),
-            hpNext_(nullptr)
-        {
-        }
-        explicit NodeWithHazardPointer(T&& data)
-            : data_(std::move(data)),
-            next_(nullptr),
-            hpNext_(nullptr)
-        {
-        }
-        ~NodeWithHazardPointer() {}
-    };
+    }
+    ~NodeWithHazardPointer() {}
+};
 
 }
 
